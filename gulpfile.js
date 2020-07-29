9717a546b7c52c05f0f6034865d7d597403abb2b
Divyansh <divi.sunway@gmail.com>
2020-07-29 17:35:28 +0530

    title

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: './src',
+        notify: false,
+        ghostMode: false,
+    });
+
+    gulp.watch("./src/*.css", function(){
+        return gulp.src("./src/*.css").pipe(browserSync.stream());
+    });
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('serve'));
