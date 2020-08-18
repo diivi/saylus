4be336866a8f41e3902827d02d4cbea6074f9476
Divyansh <divi.sunway@gmail.com>
2020-08-18 20:26:47 +0530

    prthms test

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-var myNav = document.getElementById('navbar');
-window.onscroll = function  scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.backgroundColor = "white";
-    items=document.getElementsByClassName("navlink");
-    for (var i=0; i < items.length; i++) {
-      items[i].style.color = "#BABABA";
-    }
-  } else {
-    document.getElementById("navbar").style.backgroundColor = "";
-    items=document.getElementsByClassName("navlink");
-    for (var i=0; i < items.length; i++) {
-      items[i].style.color = "white";
-    }
-  }
-}
+// var myNav = document.getElementById('navbar');
+// window.onscroll = function  scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.backgroundColor = "white";
+//     items=document.getElementsByClassName("navlink");
+//     for (var i=0; i < items.length; i++) {
+//       items[i].style.color = "#BABABA";
+//     }
+//   } else {
+//     document.getElementById("navbar").style.backgroundColor = "";
+//     items=document.getElementsByClassName("navlink");
+//     for (var i=0; i < items.length; i++) {
+//       items[i].style.color = "white";
+//     }
+//   }
+// }
