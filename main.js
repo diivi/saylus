e1e0c0dab599a3f990b7ed78d2288306411fbbcc
Divyansh <divi.sunway@gmail.com>
2020-08-18 22:01:24 +0530

    abouts without team

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-// var myNav = document.getElementById('navbar');
-// window.onscroll = function  scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.backgroundColor = "white";
-//     items=document.getElementsByClassName("navlink");
-//     for (var i=0; i < items.length; i++) {
-//       items[i].style.color = "#BABABA";
-//     }
-//   } else {
-//     document.getElementById("navbar").style.backgroundColor = "";
-//     items=document.getElementsByClassName("navlink");
-//     for (var i=0; i < items.length; i++) {
-//       items[i].style.color = "white";
-//     }
-//   }
-// }
+var myNav = document.getElementById('navbar');
+window.onscroll = function  scrollFunction() {
+  var $nav = $("#navbar");
+  var maincon=$('.centrr')
+  $nav.toggleClass('div1', $(this).scrollTop() > maincon.height()+$nav.height());
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.backgroundColor = "white";
+    items=document.getElementsByClassName("navlink");
+    for (var i=0; i < items.length; i++) {
+      items[i].style.color = "#BABABA";
+    }
+    
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "";
+    items=document.getElementsByClassName("navlink");
+    for (var i=0; i < items.length; i++) {
+      items[i].style.color = "white"; 
+    }
+  }
+}
