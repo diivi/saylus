13f5b69618564768cade902c9224d60109a0d685
Divyansh <divi.sunway@gmail.com>
2020-08-18 22:52:42 +0530

    team left nd responsv

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var myNav = document.getElementById('navbar');
 window.onscroll = function  scrollFunction() {
   var $nav = $("#navbar");
-  var maincon=$('.centrr')
+  var maincon=$('.centrr');
+  var navimg = $('nav img')
   $nav.toggleClass('div1', $(this).scrollTop() > maincon.height()+$nav.height());
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.backgroundColor = "white";
@@ -9,12 +10,13 @@ window.onscroll = function  scrollFunction() {
     for (var i=0; i < items.length; i++) {
       items[i].style.color = "#BABABA";
     }
-    
+    navimg.attr("src",'./res/topred.png')
   } else {
     document.getElementById("navbar").style.backgroundColor = "";
     items=document.getElementsByClassName("navlink");
     for (var i=0; i < items.length; i++) {
       items[i].style.color = "white"; 
     }
+    navimg.attr("src",'./res/top.png')
   }
 }
